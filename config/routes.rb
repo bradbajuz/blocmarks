fb6bf8626bf024951609419cc09a71fe21fb6ca7
bradbajuz <brad@bradballard.com>
2014-04-21 20:54:56 -0400

    Configure bootstrap and styled nav area.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Blocmarks::Application.routes.draw do
-
-
+  
   devise_for :users
-  match 'about', to: 'welcome#about', via: :get
+  
+  get "welcome/index"
+  get "welcome/about"
+  # match 'about', to: 'welcome#about', via: :get
   root 'welcome#index'
 end
