39c0661993d2bc65f9f5e75464da2e07eff30039
bradbajuz <brad@bradballard.com>
2014-04-23 21:23:05 -0400

    Started adding mailgun

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Blocmarks::Application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
 
+  post :incoming, to: 'bookmarks#create'
   match 'about', to: 'welcome#about', via: :get
   root 'welcome#index'
 end
