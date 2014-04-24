42bc89dd6001bfa84c2a5ad12d8ac47eeb381d1f
bradbajuz <brad@bradballard.com>
2014-04-23 20:39:20 -0400

    Add bookmarks controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Blocmarks::Application.routes.draw do
   
+  resources :bookmarks, only: [:index]
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  
+
   match 'about', to: 'welcome#about', via: :get
   root 'welcome#index'
 end
