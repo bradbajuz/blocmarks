4cee08aaa024dba9326fedd0302e6dd55d17f5e5
bradbajuz <brad@bradballard.com>
2014-04-26 14:45:30 -0400

    Add bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140422003618) do
+ActiveRecord::Schema.define(version: 20140426183709) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookmarks", force: true do |t|
+    t.string   "title"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
