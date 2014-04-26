d035540209e6eed28369d062746bd4670a6a601c
bradbajuz <brad@bradballard.com>
2014-04-26 15:39:37 -0400

    Messed up incoming

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Blocmarks::Application.routes.draw do
-
-  post :incoming, to: 'incoming#create'
   
-  resources :bookmarks, only: [:index]
-
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   
