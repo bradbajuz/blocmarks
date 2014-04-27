3529b889db5df771b22cf363b58f2e49f8d7d7f9
bradbajuz <brad@bradballard.com>
2014-04-27 07:26:59 -0400

    Added mailgun mailer and updated security

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Blocmarks::Application.config.secret_key_base = '5df147b2d0a05715d27961f1bf63058cad4c9d2d2c32e6dbcc565fe86e8fca79d978ea14c2c4393c7950a0efbd97a6e6b77540e3525d05e51f0eded8d85b7231'
+Blocmarks::Application.config.secret_key_base = ENV['SECRET_KEY_BASE']
