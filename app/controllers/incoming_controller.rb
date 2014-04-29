f12b3497961f2d281da29c39ade2bbbc6b09ee3c
Brad Ballard <Brad@brad-mba.local>
2014-04-29 17:02:45 -0400

    Add incoming model again

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,7 @@ class IncomingController < ApplicationController
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_filter :verify_authenticity_token, only: [:create]
 
-  def create
+  def post
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
 
@@ -12,25 +12,7 @@ class IncomingController < ApplicationController
     sender = params['sender']
     subject = params['subject']
     body_plain = params["body-plain"]
-    user = User.find_by_email(sender)
-    id = user.id
-    email = user.email
-
-    if user.bookmarks.count > 0
-      user_bookmarks = user.bookmarks
-      titles = user.bookmarks.map { |bm| bm["title"] }
-
-      if titles.include?(subject)
-        user_bookmarks = user_bookmarks.find_by_title(subject)
-        user_bookmarks.hotlists.create!(url: body_plain, user_id: id)
-      else
-        new_user_submission = user.bookmarks.create!({ title: subject, url: body_plain })
-        new_user_submission.hotlists.create!(url: body_plain, user_id: id)
-      end
-    else
-      new_submission = user.bookmarks.create!({ title: subject, url: body_plain })
-      new_submission.hotlists.create!(url: body_plain, user_id: id)
-    end
+ 
 
     # Assuming all went well. 
     head 200
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140429025356) do
+ActiveRecord::Schema.define(version: 20140429205928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,14 @@ ActiveRecord::Schema.define(version: 20140429025356) do
 
   add_index "hotlists", ["bookmark_id"], name: "index_hotlists_on_bookmark_id", using: :btree
   add_index "hotlists", ["user_id"], name: "index_hotlists_on_user_id", using: :btree
+
+  create_table "incomings", force: true do |t|
+    t.string   "sender"
+    t.string   "subject"
+    t.string   "body_plain"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
