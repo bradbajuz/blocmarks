8912d34267d0aec30f5876dd74efb96d95d21d66
bradbajuz <brad@bradballard.com>
2014-04-28 22:37:51 -0400

    Add to incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -16,7 +16,21 @@ class IncomingController < ApplicationController
     id = user.id
     email = user.email
 
-    render :text => "OK"
+    if user.bookmarks.count > 0
+      user_bookmarks = user.bookmarks
+      titles = user.bookmarks.map { |bm| bm["title"] }
+
+      if titles.include?(subject)
+        user_bookmarks = user_bookmarks.find_by_title(subject)
+        user_bookmarks.hotlists.create!(url: body_plain, user_id: id)
+      else
+        new_user_submission = user.bookmarks.create!({ title: subject, url: body_plain })
+        new_user_submission.hotlists.create!(url: body_plain, user_id: id)
+      end
+    else
+      new_submission = user.bookmarks.create!({ title: subject, url: body_plain })
+      new_submission.hotlists.create!(url: body_plain, user_id: id)
+    end
 
     # Assuming all went well. 
     head 200
