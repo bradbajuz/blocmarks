5337b622f3ad164bd79b967be2455d526bd55b2f
bradbajuz <brad@bradballard.com>
2014-04-29 17:56:23 -0400

    Update incoming controller

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,7 +3,7 @@ class IncomingController < ApplicationController
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
   skip_before_filter :verify_authenticity_token, only: [:create]
 
-  def post
+  def create
     # Take a look at these in your server logs
     # to get a sense of what you're dealing with.
 
