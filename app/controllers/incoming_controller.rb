f8d251901ceab9d9f5d5c948ec4eb5faf6a8ef86
bradbajuz <brad@bradballard.com>
2014-04-29 17:41:14 -0400

    Fix incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,7 +1,7 @@
 class IncomingController < ApplicationController
 
   # http://stackoverflow.com/questions/1177863/how-do-i-ignore-the-authenticity-token-for-specific-actions-in-rails
-  skip_before_filter :verify_authenticity_token, only: [:post]
+  skip_before_filter :verify_authenticity_token, only: [:create]
 
   def post
     # Take a look at these in your server logs
@@ -15,6 +15,22 @@ class IncomingController < ApplicationController
     user = User.find_by_email(sender)
     id = user.id
     email = user.email
+
+    if user.bookmarks.count > 0
+      user_bookmarks = user.bookmarks
+      titles = user.bookmarks.map { |bm| bm["title"] }
+
+      if titles.include?(subject)
+        user_bookmarks = user_bookmarks.find_by_title(subject)
+        user_bookmarks.hotlists.create!(url: body_plain, user_id: id)
+      else
+        new_user_submission = user.bookmarks.create!({ title: subject, url: body_plain })
+        new_user_submission.hotlists.create!(url: body_plain, user_id: id)
+      end
+    else
+      new_submission = user.bookmarks.create!({ title: subject, url: body_plain })
+      new_submission.hotlists.create!(url: body_plain, user_id: id)
+    end
  
 
     # Assuming all went well. 
--- app/models/incoming.rb
+++
@@ -1,2 +0,0 @@
-class Incoming < ActiveRecord::Base
-end
