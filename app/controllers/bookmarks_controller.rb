457d37d259c7fba2e30348f8e8808d4a71f361ef
bradbajuz <brad@bradballard.com>
2014-04-30 19:57:09 -0400

    Update bookmarks view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,4 +1,13 @@
 class BookmarksController < ApplicationController
   def index
+    @bookmark = Bookmark.new
+    @bookmarks = Bookmark.all
+    @bookmarks = current_user.bookmarks
+  end
+
+private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:title, :url)
   end
 end
