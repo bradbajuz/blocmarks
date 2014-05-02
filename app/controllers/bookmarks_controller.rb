fd03c98d733821688263c3f30d83adf9fcaaf7cb
bradbajuz <brad@bradballard.com>
2014-05-01 23:32:05 -0400

    Remove bookmarks show. Update index and controller

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -3,11 +3,17 @@ class BookmarksController < ApplicationController
   def index
     @bookmark = Bookmark.new
     @bookmarks = Bookmark.all
-    @bookmarks = current_user.bookmarks
+    if params[:title]
+      @bookmark_groups = current_user.bookmarks.where(title: params[:title]).group_by { |b| b.title }
+    else
+      @bookmark_groups = current_user.bookmarks.group_by { |b| b.title }
+    end
+
   end
 
   def show
     @bookmark = Bookmark.find(params[:id])
+    @bookmarks = Bookmark.all
   end
 
 end
