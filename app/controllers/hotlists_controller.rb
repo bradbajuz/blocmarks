a4380ed44f7d797e636a749a6295ed408f0c061a
bradbajuz <brad@bradballard.com>
2014-05-03 20:40:43 -0400

    Create hotlists view and show bookmarks

--- app/controllers/hotlists_controller.rb
+++ app/controllers/hotlists_controller.rb
@@ -1,4 +1,11 @@
 class HotlistsController < ApplicationController
-  def show
+  def index
+    
+    if params[:title]
+      @hotlist_groups = current_user.bookmarks.where(title: params[:title]).group_by { |h| h.title }
+    else
+      @hotlist_groups = current_user.bookmarks.group_by { |h| h.title }
+    end
   end
+
 end
