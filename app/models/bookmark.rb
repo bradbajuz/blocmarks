b87c6fe27e8b49e2642829431559b4b33a4fb0f5
bradbajuz <brad@bradballard.com>
2014-05-10 10:21:49 -0400

    Add desc by created at

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,7 @@ class Bookmark < ActiveRecord::Base
 
   belongs_to :user
   has_many :likes, dependent: :destroy
+
+  default_scope { order('created_at DESC') }
   
 end
--- app/models/hotlist.rb
+++ app/models/hotlist.rb
@@ -1,3 +1,5 @@
 class Hotlist < ActiveRecord::Base
   belongs_to :bookmark
+
+  default_scope { order('updated_at DESC') }
 end
