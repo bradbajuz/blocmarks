cb91e814c9aba769773769227ec386e1774017df
bradbajuz <brad@bradballard.com>
2014-05-14 22:24:52 -0400

    Fix hotlist controller group by

--- app/controllers/hotlists_controller.rb
+++ app/controllers/hotlists_controller.rb
@@ -4,7 +4,7 @@ class HotlistsController < ApplicationController
     @hotlist_groups = Bookmark.all
 
     if params[:title]
-      @hotlist_groups = @hotlist_groups.where(title: params[:title])
+      @hotlist_groups = @hotlist_groups.where(title: params[:title]).group_by { |h| h.title }
     else
       @hotlist_groups = @hotlist_groups.group_by { |h| h.title }
     end
